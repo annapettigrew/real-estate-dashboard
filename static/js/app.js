f63911597b0886ef6be750ec117bb586e5339303
Tyler Hill <tylergenehill122@gmail.com>
2022-07-06 19:00:21 -0400

    json data linked to dropdown menu

--- static/js/app.js
+++ static/js/app.js
@@ -7,6 +7,10 @@ optionChanged(d3.select("#selCity"));
 // master function to grab data and display charts based on value input
 function optionChanged(val) {
     console.log(val);
+    url = `/city/${val}`;
+    d3.json(url).then((data) => {
+        console.log(data)
+    });
 };
 
 optionChanged("29470");
