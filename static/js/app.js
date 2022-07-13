56a58ff8368fb9b07d2de60af39a9554aa09e30b
Tyler Hill <tylergenehill122@gmail.com>
2022-07-12 22:58:27 -0400

    national avg

--- static/js/app.js
+++ static/js/app.js
@@ -25,7 +25,7 @@ function optionChanged(val) {
             var bath = homes[i].homeData.baths;
             var sqFt = homes[i].homeData.sqftInfo.amount;
             var timeOnMarket = homes[i].homeData.daysOnMarket.daysOnMarket;
-
+            
             if ((timeOnMarket) !== undefined){
                 time.push(timeOnMarket.value)
             };
@@ -55,13 +55,16 @@ function optionChanged(val) {
         var cityState = homes[0].homeData.addressInfo.city + ", " + homes[0].homeData.addressInfo.state
         var meanLat = d3.mean(lats)
         var meanLon = d3.mean(lons)
-        var meanPrice = Math.round(d3.mean(homePrices))
+        var meanPrice = Math.round(d3.mean(homePrices)).toLocaleString()
+        var medianPrice = Math.round(d3.median(homePrices))
+        // price dif from natl avg
+        var priceDif = (medianPrice - 428379)
         var meanBeds = Math.round(d3.mean(beds))
         var meanBaths = Math.round(d3.mean(baths))
         var meanSqft = Math.round(d3.mean(sqFeet))
-        var meanSqftPrice = d3.mean(pricePerSqft)
+        // var meanSqftPrice = d3.mean(pricePerSqft)
 
-        cityInfo(cityState, meanPrice, meanBeds, meanBaths, meanSqft, meanSqftPrice)
+        cityInfo(cityState, meanPrice, meanBeds, meanBaths, meanSqft, medianPrice, priceDif)
         setView(meanLat,meanLon)
         createMarkers(data)
         bar(homePrices, time);
@@ -69,16 +72,55 @@ function optionChanged(val) {
     });
 };
 
-function cityInfo(cityState, meanPrice, meanBeds, meanBaths, meanSqft, meanSqftPrice) {
+function cityInfo(cityState, meanPrice, meanBeds, meanBaths, meanSqft, medianPrice, priceDif) {
     d3.select("#header").html(`
         <h4>${cityState}</h4><br />
     `)
+    
+    var color = ""
+    if (priceDif > 0){
+        color = "green"
+    };
+    if (priceDif < 0){
+        color = "red"
+    };
+
     d3.select("#cityInfo").html(`
-        <h5>Mean Price: $${meanPrice}</h5>
-        <h5>Mean Beds: ${meanBeds}</h5>
-        <h5>Mean Baths: ${meanBaths}</h5>
-        <h5>Mean sqft: ${meanSqft}</h5>
-        <h5>Mean Price per sqft: ${meanSqftPrice}</h5>
+        <table>
+            <tbody id="statTable">
+                <tr>
+                    <td></td>
+                    <td></td>
+                    <td>National Average</td>
+                </tr>
+                <tr>
+                    <td>Median Price</td>
+                    <td>$${medianPrice}</td>
+                    <td>$428,379</td>
+                    <td style="color:${color}">$${priceDif.toLocaleString()}</td>
+                </tr>
+                <tr>
+                    <td>Mean Price</td>
+                    <td>$${meanPrice}</td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td>Mean Beds</td>
+                    <td>${meanBeds}</td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td>Mean Baths</td>
+                    <td>${meanBaths}</td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td>Mean sqft</td>
+                    <td>${meanSqft}</td>
+                    <td></td>
+                </tr>
+            </tbody>
+        </table>
     `)
 };
 
